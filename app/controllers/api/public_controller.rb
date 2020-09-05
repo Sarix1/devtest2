c484262724868a7053cead174031ca5532a8630a
Sarix1 <sari.katariina.jokinen@protonmail.com>
2020-09-05 23:40:47 +0300

    Add web scraper, update fixtures and dbs, move API functions to models

--- app/controllers/api/public_controller.rb
+++ app/controllers/api/public_controller.rb
@@ -4,12 +4,41 @@ class Api::PublicController < ApplicationController
   end
 
   def get_target_groups_for_country_code
-    redirect_to controller: "TargetGroupsController",
-                    action: "get_for_country_code"
+    render json: TargetGroup.for_country_code(params[:country_code])
   end
 
   def evaluate_target
-    redirect_to controller: "EvaluateTarget",
-                    action: "evaluate_target"
+    country = Country.find_by country_code: params[:country_code]
+    panel_provider_id = country.panel_provider_id
+
+    if panel_provider_id.in?([1, 2, 3])
+      scraper = Scraper.new
+
+      if panel_provider_id == 1
+        character_a_count = scraper.count_characters("https://time.com/", "a")
+
+        render json: {data: character_a_count / 100}
+
+      elsif panel_provider_id == 2
+        array_count = scraper.count_arrays("https://openlibrary.org/search.json?q=the+lord+of+the+rings", 10)
+
+        render json: {data: array_count}
+
+      elsif panel_provider_id == 3
+        node_count = scraper.count_html_nodes("https://time.com/")
+
+        render json: {data: node_count / 100}
+      end
+
+    else
+      render json: {data: "Invalid panel provider ID"}   
+    end
   end
+
+  private
+
+    def evaluate_target_params
+      params.require(:country_code, :target_group_id, :locations)
+      params.require(:locations).permit(:id, :panel_size)
+    end
 end
